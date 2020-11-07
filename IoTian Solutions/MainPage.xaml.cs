1142ef44f9d8620a48b3579c55e15740f3fa8c09
Mohd Aftab Alam Ansari <68464806+Aftab4772@users.noreply.github.com>
2020-11-07 11:21:03 +0530

    Add new Solution

---
+++ IoTian Solutions/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IoTian_Solutions
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            appLogo.Source = Device.RuntimePlatform == Device.Android
+                ? ImageSource.FromFile("MainIcon230.png")
+                : ImageSource.FromFile("Images/MainIcon230.png");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainSearchWindow());
+        }
+
+
+    }
+}
